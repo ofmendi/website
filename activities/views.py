0d5fecad04ef262708fb36ef3f8e20c34155347f
Ömer Faruk MENDİ <omerfarukmendi@protonmail.com>
2019-01-01 19:13:03 +0300

    created ActivitiesView view for app. Writed new method; gets past events on meetup.

--- activities/views.py
+++ activities/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import os
+import json
+import pprint
+import meetup.api
+from django.views.generic.base import TemplateView
+class ActivitiesView(TemplateView):
+    template_name = "activities/activities_list.html"
 
-# Create your views here.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['msg'] = self.get_all_activities()
+        return context
+
+    def get_past_activities(self):
+        client = meetup.api.Client(os.environ.get('MEETUP_API_KEY'))
+        past_events = client.GetEvents({'group_urlname': 'python-istanbul', 'status': 'past'})
+        return past_events.__dict__
